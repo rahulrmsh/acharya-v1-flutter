e484e817bf6782d9436f4a4dc86b2ed47aa1fc02
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-03-24 11:25:01 +0530

    splash screen updated

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -10,11 +11,11 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  final height = ScreenUtil().setHeight(2400);
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context,
         designSize: Size(1080, 2400), allowFontScaling: false);
+    final height = ScreenUtil().setHeight(2400);
     return WillPopScope(
       onWillPop: () async {
         await SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop');
@@ -43,7 +44,7 @@ class _SplashScreenState extends State<SplashScreen> {
                       child: Center(
                         child: Text(
                           "Acharya",
-                          style: TextStyle(
+                          style: GoogleFonts.montserrat(
                             color: Colors.white,
                             fontSize: 50,
                             fontWeight: FontWeight.w700,
