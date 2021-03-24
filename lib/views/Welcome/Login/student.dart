2a8784f04ba2fcc4e6dccaf4ec3702633b529445
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-03-24 15:52:54 +0530

    welcome screen done

--- lib/views/Welcome/Login/student.dart
+++ lib/views/Welcome/Login/student.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class StudentLoginScreen extends StatefulWidget {
+  @override
+  _StudentLoginScreenState createState() => _StudentLoginScreenState();
+}
+
+class _StudentLoginScreenState extends State<StudentLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
