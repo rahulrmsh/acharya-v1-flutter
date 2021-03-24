20650f9b562e90b853dd0b3d92c37e5bcf2a6de0
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-03-24 12:40:09 +0530

    image added

--- lib/views/Login/welcome.dart
+++ lib/views/Login/welcome.dart
@@ -1,5 +1,7 @@
 import 'package:acharya1/utilities/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatefulWidget {
   @override
@@ -9,11 +11,49 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context,
+        designSize: Size(1080, 2400), allowFontScaling: false);
+    final height = ScreenUtil().setHeight(2400);
     return SafeArea(
       child: Scaffold(
         backgroundColor: whiteColor,
         body: Column(
-          children: [],
+          children: [
+            Center(
+              child: Image(
+                height: height * 0.4,
+                image: AssetImage('assets/images/ACH2.png'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: height * 0.04),
+              child: Center(
+                child: Text(
+                  "Lorem ipsum dolor sit amet",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Bambino',
+                    letterSpacing: 0.5,
+                    fontSize: ScreenUtil().setSp(90),
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: height * 0.01),
+              child: Center(
+                child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                  style: GoogleFonts.josefinSans(
+                    color: Colors.white,
+                    fontSize: ScreenUtil().setSp(70),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
