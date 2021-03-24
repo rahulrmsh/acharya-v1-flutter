04e3bb7fd8a132eb30128b6cebe5fae99cfa2863
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-03-24 15:53:38 +0530

    welcome screen done

--- lib/views/Welcome/Login/student.dart
+++ lib/views/Welcome/Login/student.dart
@@ -1,3 +1,4 @@
+import 'package:acharya1/utilities/constants.dart';
 import 'package:flutter/material.dart';
 
 class StudentLoginScreen extends StatefulWidget {
@@ -8,6 +9,11 @@ class StudentLoginScreen extends StatefulWidget {
 class _StudentLoginScreenState extends State<StudentLoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: whiteColor,
+        body: Column(),
+      ),
+    );
   }
 }
