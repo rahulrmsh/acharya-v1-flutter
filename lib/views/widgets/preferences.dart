dd9491e4fe73ae926dc4e531009a5d0d0f872c2e
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-03-25 01:00:28 +0530

    preferences screen added

---
+++ lib/views/widgets/preferences.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PreferencesScreen extends StatefulWidget {
+  @override
+  _PreferencesScreenState createState() => _PreferencesScreenState();
+}
+
+class _PreferencesScreenState extends State<PreferencesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
